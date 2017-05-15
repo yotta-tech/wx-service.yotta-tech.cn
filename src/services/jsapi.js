6247a1d460c6904544291df991de5d6a68c4600a
jackgit <ygjack414@hotmail.com>
2017-05-15 11:16:19 +0800

    wx-service@0.0.25

--- src/services/jsapi.js
+++ src/services/jsapi.js
@@ -1,21 +1,13 @@
 const wxService = require('wx-service')
 const jsapi = wxService.jsapi
 const cache = wxService.cache
-const randomString = wxService.utils.string.random
 
 exports.getConfig = async function (url) {
-  console.log('cache', cache)
-  console.log('url', url)
   let accessToken = cache.accessToken
   let ticket = await jsapi.getTicket(accessToken).then(r => r.ticket)
-  let nonceStr = randomString(16)
-  let timestamp = Date.now()
-  let signature = jsapi.sign(nonceStr, timestamp, ticket, url)
+  let signResult = jsapi.sign(ticket, url)
 
-  return {
-    appId: wxService.config.appId,
-    nonceStr,
-    timestamp,
-    signature
-  }
+  signResult.appId = wxService.config.appId
+  console.log('jspi.getConfig', signResult)
+  return signResult
 }
